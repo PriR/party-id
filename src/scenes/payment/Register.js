687ec60961cc5bd5156577e6e18e70e679a953e9
Priscila Silva <priscila.rodrigues@hotmail.com.br>
2020-07-12 21:02:00 +0200

    Improving credit card layout

--- src/scenes/payment/Register.js
+++ src/scenes/payment/Register.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView, StyleSheet, TextInput} from 'react-native';
 import Lottie from 'lottie-react-native';
 
 import card from '../../assets/lottie/card.json';
-// 22
+import {white} from '../../utils/colors';
+
 const Register = () => {
+  const handleOnChangeText = (id, value) => {
+    switch (id) {
+      case 'firstBlock': {
+        console.log('firstBlock');
+        break;
+      }
+      case 'secondBlock': {
+        console.log('secondBlock');
+        break;
+      }
+      case 'thirdBlock': {
+        console.log('thirdBlock');
+        break;
+      }
+      case 'fourthBlock': {
+        console.log('fourthBlock');
+        break;
+      }
+    }
+  };
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <CardView>
@@ -19,18 +41,74 @@ const Register = () => {
         />
         <CardDataView>
           <CardNumberView>
-            <Numbers>2222 2222 2222 2222</Numbers>
+            <Label>Número do cartão</Label>
+            <Row>
+              <TextInput
+                style={styles.inputNumbersCard}
+                placeholder="1234"
+                placeholderTextColor="#B3B6B7"
+                maxLength={4}
+                keyboardType="number-pad"
+                onChangeText={value => handleOnChangeText('firstBlock', value)}
+              />
+              <Space />
+              <TextInput
+                style={styles.inputNumbersCard}
+                placeholder="1234"
+                placeholderTextColor="#B3B6B7"
+                maxLength={4}
+                keyboardType="number-pad"
+                onChangeText={value => handleOnChangeText('secondBlock', value)}
+              />
+              <Space />
+              <TextInput
+                style={styles.inputNumbersCard}
+                placeholder="1234"
+                placeholderTextColor="#B3B6B7"
+                maxLength={4}
+                keyboardType="number-pad"
+                onChangeText={value => handleOnChangeText('thirdBlock', value)}
+              />
+              <Space />
+              <TextInput
+                style={styles.inputNumbersCard}
+                placeholder="1234"
+                placeholderTextColor="#B3B6B7"
+                maxLength={4}
+                keyboardType="number-pad"
+                onChangeText={value => handleOnChangeText('fourthBlock', value)}
+              />
+            </Row>
           </CardNumberView>
 
           <ContainerRow>
             <CardNameView>
-              <Name>Priscila Rodrigues da Silva</Name>
+              <Label>Nome</Label>
+              <TextInput
+                style={styles.inputText}
+                maxLength={26}
+                placeholder="Priscila Rodrigues da Silva"
+                placeholderTextColor="#B3B6B7"
+                underlineColorAndroid="rgba(0,0,0,0)"
+              />
             </CardNameView>
             <ExpirationDateView>
-              <Date>12/27</Date>
+              <Label>Data de expiração</Label>
+              <TextInput
+                style={styles.inputText}
+                maxLength={5}
+                placeholder="12/27"
+                placeholderTextColor="#B3B6B7"
+              />
             </ExpirationDateView>
             <CVVView>
-              <CVV>123</CVV>
+              <Label>CVV</Label>
+              <TextInput
+                style={styles.inputText}
+                maxLength={3}
+                placeholder="123"
+                placeholderTextColor="#B3B6B7"
+              />
             </CVVView>
           </ContainerRow>
         </CardDataView>
@@ -41,9 +119,17 @@ const Register = () => {
 
 const styles = StyleSheet.create({
   containerLottie: {
-    height: '60%',
+    height: 90,
     justifyContent: 'flex-start',
     alignItems: 'flex-start',
+  },
+  inputText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  inputNumbersCard: {
+    color: 'white',
+    fontSize: 25,
   },
 });
 
@@ -59,68 +145,61 @@ const CardView = styled.View`
 const CardDataView = styled.View`
   flex: 1;
   margin-left: 10px;
+  margin-top: 10px;
+`;
+
+const Space = styled.View`
+  margin-left: 10px;
 `;
 
 const CardNumberView = styled.View`
-  flex-direction: row;
+  flex-direction: column;
   margin-bottom: 10px;
   margin-top: 10px;
+  margin-right: 5px;
   justify-content: flex-start;
-  align-items: center;
+  align-items: flex-start;
 `;
 
 const ContainerRow = styled.View`
   flex-direction: row;
   justify-content: space-between;
+  align-items: baseline;
+`;
+
+const Row = styled.View`
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
 `;
 
 const CardNameView = styled.View`
   flex: 1;
-  flex-direction: row;
-  flex-grow: 3;
+  flex-direction: column;
+  flex-grow: 4;
   margin-bottom: 10px;
   justify-content: flex-start;
 `;
 
 const ExpirationDateView = styled.View`
   flex: 1;
+  flex-grow: 2;
   margin-bottom: 10px;
   align-items: flex-start;
 `;
 
 const CVVView = styled.View`
   flex: 1;
+  flex-direction: column;
   margin-bottom: 10px;
   margin-right: 10px;
   align-items: center;
 `;
 
-const Numbers = styled.Text`
+const Label = styled.Text`
   color: white;
   font-weight: bold;
-  opacity: 0.5;
-  font-size: 25px;
-`;
-
-const Name = styled.Text`
-  color: white;
-  font-weight: bold;
-  opacity: 0.5;
-  font-size: 15px;
-`;
-
-const Date = styled.Text`
-  color: white;
-  font-weight: bold;
-  opacity: 0.5;
-  font-size: 15px;
-`;
-
-const CVV = styled.Text`
-  color: white;
-  font-weight: bold;
-  opacity: 0.5;
-  font-size: 15px;
+  font-size: 10px;
 `;
 
 export default Register;
--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,6 +1,7 @@
 export const colors = {
   black: '#000',
   white: '#fff',
+  gray: '#B3B6B7',
 
   darkPurple: '#513e6f',
   lightPurple: '#ccccff',
